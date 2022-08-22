bf65026ea51f5202ee065379fdb180ab35fa2dc0
whitney <whitney.njogu@student.moringaschool.com>
2022-08-22 23:15:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(CardsArr,cere){
+    const thankYouCards=[];
+    for (let i=0; i< CardsArr.length; i++) {
+        thankYouCards.push(`Thank you, ${CardsArr[i]}, for the wonderful ${cere} gift!`)
+    }
+    return thankYouCards;
+}
+
+function countDown(startNo){
+    while (startNo > 0) {
+        console.log(startNo);
+        startNo -= 1;
+    }
+    
+        console.log(startNo);
+}
